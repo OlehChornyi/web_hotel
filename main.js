8addd9c9d449796c6b50b02437e818b1bf7ef930
oleh <phd.chornyi@gmail.com>
2024-10-14 15:24:26 +0300

    3rd: room section

--- main.js
+++ main.js
@@ -20,7 +20,8 @@ const scrollRevealOption = {
     duration: 1000,
 };
 
-//header container
+// header container
+
 ScrollReveal().reveal(".header__container p", {
     ...scrollRevealOption,
 });
@@ -28,4 +29,38 @@ ScrollReveal().reveal(".header__container p", {
 ScrollReveal().reveal(".header__container h1", {
     ...scrollRevealOption,
     delay: 500,
+});
+
+// about container
+
+ScrollReveal().reveal(".about__image img", {
+    ...scrollRevealOption,
+    origin: "left",
+});
+
+ScrollReveal().reveal(".about__content .section__subheader", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+
+ScrollReveal().reveal(".about__content .section__header", {
+    ...scrollRevealOption,
+    delay: 1000,
+});
+
+ScrollReveal().reveal(".about__content .section__description", {
+    ...scrollRevealOption,
+    delay: 1500,
+});
+
+ScrollReveal().reveal(".about__btn", {
+    ...scrollRevealOption,
+    delay: 2000,
+});
+
+// room container
+
+ScrollReveal().reveal(".room__card", {
+    ...scrollRevealOption,
+    interval: 500,
 });
